d4f42bfb0d6871dbfb714dd5eb58d20309fb4d32
Iqbal Farabi <farabi.iqbal@gmail.com>
2017-10-24 12:39:45 +0700

    Organizing controller spec

--- spec/factories/food.rb
+++ spec/factories/food.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
   factory :food do
     name { Faker::Food.dish }
-    description "Betawi style steamed rice cooked in coconut milk. Delicious!"
+    description { Faker::Food.ingridient }
+    price 10000.0
+  end
+
+  factory :invalid_food do
+    name nil
+    description nil
     price 10000.0
   end
 end
