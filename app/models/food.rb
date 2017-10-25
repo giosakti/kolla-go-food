f670452443e7bfe59ec29d153484e740f46b9851
Iqbal Farabi <farabi.iqbal@gmail.com>
2017-10-25 22:23:45 +0700

    Adding has_many line_items for Cart and Food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,6 @@
 class Food < ApplicationRecord
+  has_many :line_items
+  
   validates :name, :description, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :name, uniqueness: true
