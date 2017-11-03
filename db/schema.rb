219f03a6a3b3cf198f13ee2ec443e805fa09a534
Iqbal Farabi <farabi.iqbal@gmail.com>
2017-11-03 07:20:22 +0700

    Adding Order mailer and its specs

--- db/schema.rb
+++ db/schema.rb
@@ -17,12 +17,6 @@ ActiveRecord::Schema.define(version: 20171101120121) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "foods", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -30,7 +24,6 @@ ActiveRecord::Schema.define(version: 20171101120121) do
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "category_id"
   end
 
   create_table "line_items", force: :cascade do |t|
