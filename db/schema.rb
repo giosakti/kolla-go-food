e7400a4b41bd7c8c975965f3f361cc3e581740fa
Iqbal Farabi <farabi.iqbal@gmail.com>
2017-11-10 08:01:06 +0700

    Adding Tag model and its spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106075007) do
+ActiveRecord::Schema.define(version: 20171110005843) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(version: 20171106075007) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "voucher_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
